10c11ca5a5651d8018a2b8c0bebe222fd996d403
Marchand <marchanp@uqat.ca>
2018-08-31 08:59:14 -0400

    Labo 2

--- donnees_originales/kejim_data_prep.R
+++ donnees_originales/kejim_data_prep.R
@@ -13,3 +13,13 @@ kejim <- filter(kejim, Etat %in% c("AS", "AL")) %>%
 kejim <- kejim[complete.cases(kejim),]
 
 write_csv(kejim, "donnees/cours1_kejimkujik.csv")
+
+
+kejim_dict <- read_csv("donnees_originales/Kejimkujik_NP_Forest_Trees_2004-2014_data_dictionary.csv",
+                       locale = locale(encoding = "ISO-8859-1"))
+colnames(kejim_dict) <- c("champ_en", "champ_fr", "valeur", "desc_en", "desc_fr")
+
+kejim_dict <- filter(kejim_dict, champ_fr == "Espèce") %>%
+    select(Espece = valeur, Nom_Espece = desc_fr, Species_Name = desc_en)
+
+write_csv(kejim_dict, "donnees/codes_especes.csv")
